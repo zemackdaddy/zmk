e6ed59e200d6c36346b97dcfd89a41737b64eacb
Peter Johanson <peter@peterjohanson.com>
2021-09-01 03:49:18 +0000

    refactor(sensors): Sensor event channel data, resolution tweaks.
    * Refactor sensor events to include channel data,
      necessary for prop split encoders, and avoiding duplicate calls,
      to fetch channel data twice, etc.
    * More consistent behavior driver API.
    * Allow setting activation resolution at the behavior level optionally.

--- app/src/behaviors/behavior_sensor_rotate_key_press.c
+++ app/src/behaviors/behavior_sensor_rotate_key_press.c
@@ -11,6 +11,7 @@
 #include <logging/log.h>
 
 #include <drivers/sensor.h>
+#include <zmk/sensors.h>
 #include <zmk/event_manager.h>
 #include <zmk/events/keycode_state_changed.h>
 
@@ -18,49 +19,72 @@ LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 
 #if DT_HAS_COMPAT_STATUS_OKAY(DT_DRV_COMPAT)
 
+struct behavior_sensor_rotate_key_press_cfg {
+    uint8_t activation_resolution;
+};
+
+struct behavior_sensor_rotate_key_press_sensor_data {
+    int32_t remainder[ZMK_KEYMAP_SENSORS_LEN];
+};
+
 static int behavior_sensor_rotate_key_press_init(const struct device *dev) { return 0; };
 
 static int on_sensor_binding_triggered(struct zmk_behavior_binding *binding,
-                                       const struct device *sensor, int64_t timestamp) {
-    struct sensor_value value;
-    int err;
+                                       struct zmk_behavior_binding_event event,
+                                       size_t channel_data_size,
+                                       const struct zmk_sensor_channel_data *channel_data) {
+    const struct device *behavior_dev = device_get_binding(binding->behavior_dev);
+    const struct sensor_value *value = &channel_data[0].value;
+    const struct behavior_sensor_rotate_key_press_cfg *cfg = behavior_dev->config;
+    struct behavior_sensor_rotate_key_press_sensor_data *data = behavior_dev->data;
+
     uint32_t keycode;
-    LOG_DBG("inc keycode 0x%02X dec keycode 0x%02X", binding->param1, binding->param2);
 
-    err = sensor_channel_get(sensor, SENSOR_CHAN_ROTATION, &value);
+    data->remainder[event.position] += value->val1;
 
-    if (err) {
-        LOG_WRN("Failed to ge sensor rotation value: %d", err);
-        return err;
-    }
+    int8_t triggers = data->remainder[event.position] / cfg->activation_resolution;
+    data->remainder[event.position] %= cfg->activation_resolution;
 
-    switch (value.val1) {
-    case 1:
+    LOG_DBG("value: %d, remainder: %d triggers: %d inc keycode 0x%02X dec keycode 0x%02X",
+            value->val1, data->remainder[event.position], triggers, binding->param1,
+            binding->param2);
+
+    if (triggers > 0) {
         keycode = binding->param1;
-        break;
-    case -1:
+    } else if (triggers < 0) {
         keycode = binding->param2;
-        break;
-    default:
-        return -ENOTSUP;
+        triggers = -triggers;
+    } else {
+        return 0;
     }
 
     LOG_DBG("SEND %d", keycode);
 
-    ZMK_EVENT_RAISE(zmk_keycode_state_changed_from_encoded(keycode, true, timestamp));
+    for (int i = 0; i < triggers; i++) {
+        ZMK_EVENT_RAISE(zmk_keycode_state_changed_from_encoded(keycode, true, event.timestamp));
 
-    // TODO: Better way to do this?
-    k_msleep(5);
+        // TODO: Better way to do this?
+        k_msleep(5);
 
-    return ZMK_EVENT_RAISE(zmk_keycode_state_changed_from_encoded(keycode, false, timestamp));
+        ZMK_EVENT_RAISE(zmk_keycode_state_changed_from_encoded(keycode, false, event.timestamp));
+    }
+
+    return 0;
 }
 
 static const struct behavior_driver_api behavior_sensor_rotate_key_press_driver_api = {
     .sensor_binding_triggered = on_sensor_binding_triggered};
 
 #define KP_INST(n)                                                                                 \
-    DEVICE_DT_INST_DEFINE(n, behavior_sensor_rotate_key_press_init, device_pm_control_nop, NULL,   \
-                          NULL, APPLICATION, CONFIG_KERNEL_INIT_PRIORITY_DEFAULT,                  \
+    static const struct behavior_sensor_rotate_key_press_cfg                                       \
+        behavior_sensor_rotate_key_press_cfg_##n = {                                               \
+            .activation_resolution = DT_INST_PROP_OR(n, activation_resolution, 1)};                \
+    static struct behavior_sensor_rotate_key_press_sensor_data                                     \
+        behavior_sensor_rotate_key_press_sensor_data_##n;                                          \
+    DEVICE_DT_INST_DEFINE(n, behavior_sensor_rotate_key_press_init, device_pm_control_nop,         \
+                          &behavior_sensor_rotate_key_press_sensor_data_##n,                       \
+                          &behavior_sensor_rotate_key_press_cfg_##n, APPLICATION,                  \
+                          CONFIG_KERNEL_INIT_PRIORITY_DEFAULT,                                     \
                           &behavior_sensor_rotate_key_press_driver_api);
 
 DT_INST_FOREACH_STATUS_OKAY(KP_INST)
