c969c25ffc846043a7daa71ed70cc4908ffe025f
ReFil <31960031+ReFil@users.noreply.github.com>
2022-01-30 20:50:57 +0000

    Peripheral LED updates

--- app/include/zmk/split/bluetooth/central.h
+++ app/include/zmk/split/bluetooth/central.h
@@ -3,6 +3,9 @@
 
 #include <bluetooth/addr.h>
 #include <zmk/behavior.h>
+#include <zmk/rgb_underglow.h>
 
 int zmk_split_bt_invoke_behavior(uint8_t source, struct zmk_behavior_binding *binding,
                                  struct zmk_behavior_binding_event event, bool state);
+
+int zmk_split_bt_update_led(struct zmk_periph_led *periph);
