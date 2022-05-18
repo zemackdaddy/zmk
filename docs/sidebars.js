041f32f0ed2988eabeb2a2aac3c5d64f6fe59f93
Shawn Meier <shawn.meier@colorado.edu>
2022-05-17 18:01:19 -0600

    changes from krikun98/mouse-pr rebased on zmkfirmware/main
    ifdefs for link errors when compiling non-central, some includes, other minor hacks to compile, cleanup is needed later
    
    fix hid deprication warnings with crides 6ca7298730859ef2ebab1c970cac7f9550bef14b, add missing method declaration to hog.h
    
    move ifdefs to get rid of wunused warning
    
    better ifdef to handle non split kb
    
    update mouse code from old macro CONFIG_ZMK_SPLIT_BLE_ROLE_CENTRAL to new CONFIG_ZMK_SPLIT_ROLE_CENTRAL

--- docs/sidebars.js
+++ docs/sidebars.js
@@ -35,6 +35,7 @@ module.exports = {
       "behaviors/tap-dance",
       "behaviors/caps-word",
       "behaviors/key-repeat",
+      "behaviors/mouse-emulation",
       "behaviors/reset",
       "behaviors/bluetooth",
       "behaviors/outputs",
