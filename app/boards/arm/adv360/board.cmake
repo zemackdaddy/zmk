fa377564ec185f17f9b786ef0e7661beffdaaaab
ReFil <31960031+ReFil@users.noreply.github.com>
2022-10-17 21:21:59 +0100

    strgh

---
+++ app/boards/arm/adv360/board.cmake
@@ -0,0 +1,9 @@
+#
+# Copyright (c) 2021 Polarity Works
+# SPDX-License-Identifier: MIT
+#
+
+set(OPENOCD_NRF5_SUBFAMILY nrf52)
+board_runner_args(nrfjprog "--nrf-family=NRF52" "--softreset")
+include(${ZEPHYR_BASE}/boards/common/nrfjprog.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/openocd-nrf5.board.cmake)
